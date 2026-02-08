92994d5775d3294a7dcc352d6f272e5afa5ec119
Sumon-59 <sumonbanapole01795429848@gmail.com>
2026-02-08 21:40:29 +0600

    feat: add logging with Log_LEVEL env var

--- src/calculator/cli.py
+++ src/calculator/cli.py
@@ -1,27 +1,43 @@
 from calculator.core import add, sub, mul, div
+from calculator.logger import setup_logger
 
 def main():
-    print("===Simple Calculator===")
+    logger = setup_logger()
+    logger.info("Calculator started")
+    
+    try:
+        a = float(input("Enter first number: "))
+        b = float(input("Enter second number "))
+        
+        op = input("Operation (+ - * /): ").strip()
 
-    a = float(input("Enter first number: "))
-    b = float(input("Enter second number "))
+        logger.debug("Inputs: a=%s b=%s op=%s",a, b, op)
+        
+        if op =="+":
+            result = add(a,b)
+    
+        elif op =="-":
+            result = sub(a,b)
+    
+        elif op =="*":
+            result = mul(a,b)
+        
+        elif op =="/":
+            result = div(a,b)
+        else:
+            print("Invalid operation")
+            logger.warning("Invalid operation entered: %s", op)
+            return
+        print("Result:",result)
+        logger.info("Success result=%s",result)
+    
+    except ValueError as e:
+        print("Error:",e)
+        logger.exception("ValueError happened")
 
-    op = input("Operation (+ - * /): ")
-
-    if op =="+":
-        print("Result: ",add(a,b))
-    
-    elif op =="-":
-        print("Result: ",sub(a,b))
-    
-    elif op =="*":
-        print("Result: ",mul(a,b))
-    
-    elif op =="/":
-        print("Result: ",div(a,b))
-    
-    else:
-        print("Invalid operation")
+    except Exception:
+        print("Unexpected error occurred.")
+        logger.exception("Unexpected error")
 
 if __name__ == "__main__":
     main()
